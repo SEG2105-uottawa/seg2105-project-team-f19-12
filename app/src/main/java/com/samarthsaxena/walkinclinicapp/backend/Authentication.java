a0d4f3f39b2e7aa87fd438436087622a7707e709
samarthks <samarthk0105@gmail.com>
2019-10-18 14:07:33 -0400

    hashing done

--- app/src/main/java/com/samarthsaxena/walkinclinicapp/backend/Authentication.java
+++ app/src/main/java/com/samarthsaxena/walkinclinicapp/backend/Authentication.java
@@ -2,6 +2,9 @@ package com.samarthsaxena.walkinclinicapp.backend;
 
 import com.samarthsaxena.walkinclinicapp.backend.models.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Authentication {
 
     public static User login() {
@@ -13,4 +16,29 @@ public class Authentication {
         // TODO
         return null;
     }
+
+    public static String getHash(String password) {
+        try {
+            MessageDigest digest = null;
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e1) {
+                e1.printStackTrace();
+            }
+            digest.reset();
+            return convert(digest.digest(password.getBytes()));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static String convert(byte[] data) {
+        StringBuilder x = new StringBuilder(data.length * 2);
+        for (byte b : data)
+            x.append(String.format("%02x", b & 0xFF));
+        return x.toString();
+    }
 }
+
+
+
