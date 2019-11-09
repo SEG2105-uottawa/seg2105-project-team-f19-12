55f9164212523f0fbaa40831c40980e0a8333ea0
samarthks <samarthk0105@gmail.com>
2019-11-08 19:52:44 -0500

    all data retrieval from database

--- app/src/main/java/com/samarthsaxena/walkinclinicapp/backend/models/Admin.java
+++ app/src/main/java/com/samarthsaxena/walkinclinicapp/backend/models/Admin.java
@@ -1,10 +1,14 @@
 package com.samarthsaxena.walkinclinicapp.backend.models;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 
 
 
@@ -16,14 +20,48 @@ public class Admin extends User {
         this.type = "admin";
     }
 
-    public boolean deleteaccount(String type){
+    public boolean deleteaccount(String type) {
 
         DatabaseReference dR = FirebaseDatabase.getInstance().getReference("Users").child(type);
         dR.removeValue();
         return true;
     }
 
-        }
+
+    public static ArrayList<User> alldata() {
+
+        final ArrayList<User> user = new ArrayList<User>();
+
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference("Users");
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user.clear();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User artist = snapshot.getValue(User.class);
+                        user.add(artist);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        dbref.addListenerForSingleValueEvent(valueEventListener);
+
+        return user;
+    }
+
+}
+
+
+
 
 
 
