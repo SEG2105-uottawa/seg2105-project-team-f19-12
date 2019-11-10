be6275b13c5b91bf7ffc66e3c41477dd79ea49b8
Abhuy016 <abhuy016@uottawa.ca>
2019-11-09 22:27:47 -0500

    set up managing service activity

--- app/src/main/java/com/samarthsaxena/walkinclinicapp/frontend/Admin/ManageServiceActivity.java
+++ app/src/main/java/com/samarthsaxena/walkinclinicapp/frontend/Admin/ManageServiceActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.samarthsaxena.walkinclinicapp.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ManageServiceActivity extends AppCompatActivity {
 
@@ -16,9 +20,22 @@ public class ManageServiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_services);
+        setContentView(R.layout.activity_manage_users);
 
         homeButton = findViewById(R.id.backButton);
+
+        // Replace fake users with actual users from db
+        String services[] = {"Triage", "X-ray", "Check-up"};
+
+        String roles[] = {"role1", "role2", "role3"};
+
+        ArrayList<String> serviceList = new ArrayList<String>(Arrays.asList(services));
+
+        ArrayList<String> roleList = new ArrayList<String>(Arrays.asList(roles));
+
+        CustomListAdapterServices adapter = new CustomListAdapterServices(this, serviceList, roleList);
+        ListView listView = (ListView) findViewById(R.id.serviceList);
+        listView.setAdapter(adapter);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +44,7 @@ public class ManageServiceActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-
-
     }
+
+
 }
