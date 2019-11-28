0e3b852cdd396d54fa0b3cb7893455776c7ed0d7
samarthks <samarthk0105@gmail.com>
2019-11-27 23:03:00 -0500

    layout

--- app/src/main/java/com/samarthsaxena/walkinclinicapp/frontend/Patients/PatientActivity.java
+++ app/src/main/java/com/samarthsaxena/walkinclinicapp/frontend/Patients/PatientActivity.java
@@ -22,7 +22,7 @@ public class PatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_employee);
+        setContentView(R.layout.activity_patient);
 
         welcomeText = findViewById(R.id.welcome);
         bookButton = findViewById(R.id.bookButton);
