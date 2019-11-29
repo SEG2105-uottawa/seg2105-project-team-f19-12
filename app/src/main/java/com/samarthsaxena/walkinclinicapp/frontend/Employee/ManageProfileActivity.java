246e0f0aece46bd47da395066f745133481bc72c
MelvinSam2000 <melvinsamxd@gmail.com>
2019-11-29 10:45:20 -0500

    Fixed employee functionality crashes by setting constraints

--- app/src/main/java/com/samarthsaxena/walkinclinicapp/frontend/Employee/ManageProfileActivity.java
+++ app/src/main/java/com/samarthsaxena/walkinclinicapp/frontend/Employee/ManageProfileActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.samarthsaxena.walkinclinicapp.R;
+import com.samarthsaxena.walkinclinicapp.backend.MyCallback;
+import com.samarthsaxena.walkinclinicapp.backend.models.Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class ManageProfileActivity extends AppCompatActivity {
     private TextView welcomeText;
@@ -36,9 +41,25 @@ public class ManageProfileActivity extends AppCompatActivity {
         addbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Intent myIntent = new Intent(ManageProfileActivity.this, ProfileActivity.class);
-                myIntent.putExtra("EXTRA_USERNAME", username);
-                startActivity(myIntent);
+                Profile.dbGetAll(Profile.PROFILE_USER_STRING, username, new MyCallback() {
+                    @Override
+                    public void onCallback(Object value) {
+                        ArrayList<Profile> profiles = (ArrayList<Profile>) value;
+                        if (!profiles.isEmpty()) {
+                            sendMessage("This employee already has a profile!");
+                            return;
+                        } else {
+                            Intent myIntent = new Intent(ManageProfileActivity.this, ProfileActivity.class);
+                            myIntent.putExtra("EXTRA_USERNAME", username);
+                            startActivity(myIntent);
+                        }
+                    }
+
+                    @Override
+                    public void exceptionHandler(String message) {
+
+                    }
+                });
             }
         });
 
@@ -56,12 +77,32 @@ public class ManageProfileActivity extends AppCompatActivity {
         viewbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Intent myIntent = new Intent(ManageProfileActivity.this, ViewEmployeeProfile.class);
-                myIntent.putExtra("EXTRA_USERNAME", username);
-                startActivity(myIntent);
+                Profile.dbGetAll(Profile.PROFILE_USER_STRING, username, new MyCallback() {
+                    @Override
+                    public void onCallback(Object value) {
+                        ArrayList<Profile> profiles = (ArrayList<Profile>) value;
+                        if (profiles.isEmpty()) {
+                            sendMessage("This employee has no profile yet!");
+                            return;
+                        } else {
+                            Intent myIntent = new Intent(ManageProfileActivity.this, ViewEmployeeProfile.class);
+                            myIntent.putExtra("EXTRA_USERNAME", username);
+                            startActivity(myIntent);
+                        }
+                    }
+
+                    @Override
+                    public void exceptionHandler(String message) {
+
+                    }
+                });
             }
         });
 
+    }
+
+    public void sendMessage(String message) {
+        Toast.makeText(ManageProfileActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
 
